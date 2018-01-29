d6388b4de165c536d77ff33a15b850ceab4e7f30
Fabio Casiraghi <casifabio@hotmail.it>
2018-01-30 00:13:56 +0100

    Added some rules

--- lisp/clausify.lisp
+++ lisp/clausify.lisp
@@ -11,54 +11,68 @@
   (cons (gentemp "SF-") args))
 
 (defun skolem-function (args)
-  (apply #’skolem-function* args))
+  (apply #'skolem-function* args))
 
 ;; funzioni principali
 
 (defun as-cnf (fbf)
-  ...
+  ;;...
   )
 
 (defun is-horn (fbf)
-  ...
+  ;;...
   )
 
 ;; rimozione regole di implicazione
 ;; passaggio 1 dell'algoritmo
 
 (defun rm-implication (fbf)
-  ...
+  ;;...
   )
 
 ;; riduzione delle negazioni
 ;; passaggio 2 dell'algoritmo
 
 (defun rd-negation (fbf)
-  ...
+  ;;...
   )
 
 ;; skolemizzazione
 ;; passaggio 3 dell'algoritmo
 
 (defun skolemization (fbf)
-  ...
+  ;;...
   )
 
 ;; semplificazione degli universali
 ;;passaggio 4 dell'algoritmo
 
 (defun un-semplification (fbf)
-  ...
+  ;;...
   )
 
 ;; distribuzione dell'or
 ;; passaggio 5 dell'algoritmo
 
 (defun rm-or (fbf)
-  ...
+  ;;...
   )
 
+;;======================================================
+					;REGOLE
+;;======================================================
 
+(defun term (expr)
+  (or
+   (const expr)
+   (variablep expr)
+   (funct expr)
+   )
+  )
 
-
-
+(defun const (expr)
+  (or
+   (number expr)
+   (id expr)
+   )
+  )
